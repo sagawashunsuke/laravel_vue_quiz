126583d92826f9d52a75d5091ae52cf2094dd10a
sagawashunsuke <shunkurin73@gmail.com>
2020-12-08 16:56:16 +0900

    会員登録機能見た目コーディング

--- resources/js/router.js
+++ resources/js/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './components/page/Home'
 import Quiz from './components/page/Quiz'
+import Register from './components/page/Register'
+
 
 Vue.use(Router)
 
@@ -17,6 +19,11 @@ export default new Router({
       path: '/quiz',
       name: 'quiz',
       component: Quiz
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     },
   ]
 })
