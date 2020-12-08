34e76c95d8795f76a5cddcccd3f95fb398022c3f
sagawashunsuke <shunkurin73@gmail.com>
2020-12-08 10:14:43 +0900

    実装準備Axios,Database設定

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,11 @@
 let mix = require('laravel-mix');
 
 mix.js('resources/js/home.js', 'public/js')
-    .js('resources/js/quiz.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css', {
-      implementation: require('node-sass')
-    });
+  .js('resources/js/quiz.js', 'public/js')
+  .js('resources/js/app.js', 'public/js')
+  .sass('resources/sass/app.scss', 'public/css', {
+    implementation: require('node-sass')
+  });
 
 mix.browserSync({
   files: [
